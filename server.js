7bf569b5cb85c116b8a5f638c915f549c945043f
tanan_puengjesada <08tanun403@gmail.com>
2022-01-26 13:58:15 +0700

    first commit

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ app.get("/", (req, res)=>{
     res.json({result: "ok", data:[1,2,3,4,5]})
 })
 
+app.post('/api/broadcast', (req, res) => {
+    if(!req.body.name || req.body.name.length === 0 ){
+        res.status(400).send('Name Required !!!');
+        return;
+    }
+    const testData = {
+        id: 0,
+        name: req.body.name
+    }
+    res.send(testData);
+})
+
 app.listen(PORT, ()=>{
     console.log(`Serer is running. ${PORT}`)
 })
