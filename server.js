e5350f7a239f68970ef06592e0991d02a4a6c17e
tanan_puengjesada <08tanun403@gmail.com>
2022-01-31 13:45:00 +0700

    update server.js

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.json());
 
+const axios = require('axios');
+const requestltLib = require('request');
+
+const lineToken = "Bearer 7PKzEpl7hYP0nmB6wMDUutMWAVREoBCgDhgY5vjDMWXE4o4K7NpzgknM9UxmAoj6/ZAvbvjidg8XTBbQRNEkl7IuUwkkH9GNb67TRXJIXi8aO/6P/nW8xDrIwUjKbOZFJx3m33mIdKBSjSGNiWVKCgdB04t89/1O/w1cDnyilFU=";
+
+
 app.get("/", (req, res)=>{
-    res.json({result: "ok !!!!!!!", data:[1,2,3,4,5]})
+    res.send('Hello 1.001');
 })
 
 app.post('/api/broadcast', (req, res) => {
@@ -18,6 +24,33 @@ app.post('/api/broadcast', (req, res) => {
         name: req.body.name
     }
     res.json(testData);
+})
+
+app.post('/api/push-message', (req, res) => {
+    // let messagesJson = req.body.messagesJson;
+    // const lineUid = 'U20a1f124962f1f5a1cbb026ba732004b';
+    // let lineBody = {
+    //     "to": req.body.lineUID,
+    //     "messages":[{
+    //         "type":"text",
+    //         "text": req.body.lineText
+    //     }]
+    //   };
+    let lineBody = {
+        "to": req.body.lineUID,
+        "messages":[req.body.lineMessagesJdon]
+      };
+      requestltLib.post({
+        headers:{
+          'Content-Type': 'application/json',
+          'Authorization': lineToken
+        },
+        url: 'https://api.line.me/v2/bot/message/push',
+        body: JSON.stringify(lineBody)
+      });
+    //res.json({result: "Status: 200 OK!!!!!!", lineUid: lineUid})
+    const final = {result: "Status: 200 OK 1.003" , lineUid:req.body.lineUID}
+    res.send(final);
 })
 
 app.listen(PORT, ()=>{
